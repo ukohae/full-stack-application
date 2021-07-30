ee63ba5c3338990adbf72a4ae203626889aa82a7
Emmanuel Ukoha <ukohae39@gmail.com>
2021-07-30 08:44:25 -0400

    Creating full stack application

---
+++ frontend/app/mobile/src/modules/common/Centerize/index.js
@@ -0,0 +1,13 @@
+// Imports
+import React from 'react'
+import {View} from 'react-native'
+
+// UI Imports
+import styles from './styles'
+
+// Component
+const Centerize = (props) => (
+  <View style={styles.container}>{props.children}</View>
+)
+
+export default Centerize
